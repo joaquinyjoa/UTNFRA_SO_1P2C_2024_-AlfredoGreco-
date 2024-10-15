db9e752f5f79a16c462bf42f58391fdddb9ab2db
joaquinyjoa <joaquinalfredogreco@gmail.com>
2024-10-15 15:29:24 +0000

    Montaje sdc9

--- RTA_SCRIPT_Examen_20241015/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241015/Punto_B.sh
@@ -16,3 +16,4 @@ sudo mount /dev/sdc5 /Examenes-UTN/alumno_2/parcial_1/
 sudo mount /dev/sdc6 /Examenes-UTN/alumno_2/parcial_2/
 sudo mount /dev/sdc7 /Examenes-UTN/alumno_2/parcial_3/
 sudo mount /dev/sdc8 /Examenes-UTN/alumno_3/parcial_1/
+sudo mount /dev/sdc9 /Examenes-UTN/alumno_3/parcial_2/
