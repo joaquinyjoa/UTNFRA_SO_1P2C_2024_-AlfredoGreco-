88b3fc6d41452303417ea62c317d2744ec61feb8
joaquinyjoa <joaquinalfredogreco@gmail.com>
2024-10-15 22:43:38 +0000

    Segundo usuario con contraseña del primero

--- RTA_SCRIPT_Examen_20241015/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241015/Punto_C.sh
@@ -2,3 +2,4 @@ sudo groupadd p1c2_2024_gAlumno
 sudo groupadd p1c2_2024_gProfesores
 sudo useradd -m -s /bin/bash -G p1c2_2024_gAlumno p1c2_2024_A1
 sudo passwd p1c2_2024_A1
+sudo useradd -m -s /bin/bash -G p1c2_2024_gAlumno -p $y$j9T$bh5YqA58p2TOeYKRpitlU1$U.cspPLScu.WOCf2pqFDX5/WdoRu0lzqEgO002xQVQ9 p1c2_2024_A2
