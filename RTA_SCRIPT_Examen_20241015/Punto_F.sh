512aa20188af8f4faebcb9dbd88f8842fe4fd6e7
joaquinyjoa <joaquinalfredogreco@gmail.com>
2024-10-16 02:23:50 +0000

    Mi usuario texto

--- RTA_SCRIPT_Examen_20241015/Punto_F.sh
+++ RTA_SCRIPT_Examen_20241015/Punto_F.sh
@@ -1,2 +1,6 @@
 sudo echo Mi IP publica es: >> repogit/UTNFRA_SO_1P2C_2024_-AlfredoGreco-/RTA_ARCHIVOS_Examen_20241015/Filtro_Avanzado.txt
 sudo curl -s ifconfig.me >> repogit/UTNFRA_SO_1P2C_2024_-AlfredoGreco-/RTA_ARCHIVOS_Examen_20241015/Filtro_Avanzado.txt
+Mi usuario es:
+vagrant
+echo Mi usuario es: >> repogit/UTNFRA_SO_1P2C_2024_-AlfredoGreco-/RTA_ARCHIVOS_Examen_20241015/Filtro_Avanzado.txt
+whoami >> repogit/UTNFRA_SO_1P2C_2024_-AlfredoGreco-/RTA_ARCHIVOS_Examen_20241015/Filtro_Avanzado.txt
