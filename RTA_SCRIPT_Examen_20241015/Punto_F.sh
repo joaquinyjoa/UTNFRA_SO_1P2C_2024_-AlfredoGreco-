eb25248629f318155c8c48d061f66b29a8faf387
joaquinyjoa <joaquinalfredogreco@gmail.com>
2024-10-16 02:28:48 +0000

    URL del repo

--- RTA_SCRIPT_Examen_20241015/Punto_F.sh
+++ RTA_SCRIPT_Examen_20241015/Punto_F.sh
@@ -6,3 +6,5 @@ echo Mi usuario es: >> repogit/UTNFRA_SO_1P2C_2024_-AlfredoGreco-/RTA_ARCHIVOS_E
 whoami >> repogit/UTNFRA_SO_1P2C_2024_-AlfredoGreco-/RTA_ARCHIVOS_Examen_20241015/Filtro_Avanzado.txt
 echo El Hash de mi usuario es: >> repogit/UTNFRA_SO_1P2C_2024_-AlfredoGreco-/RTA_ARCHIVOS_Examen_20241015/Filtro_Avanzado.txt
 sudo grep vagrant /etc/shadow | awk -F ':' '{print }' >> repogit/UTNFRA_SO_1P2C_2024_-AlfredoGreco-/RTA_ARCHIVOS_Examen_20241015/Filtro_Avanzado.txt
+echo La URL de mi repositorio es: >> repogit/UTNFRA_SO_1P2C_2024_-AlfredoGreco-/RTA_ARCHIVOS_Examen_20241015/Filtro_Avanzado.txt
+git -C ~/repogit/UTNFRA_SO_1P2C_2024_-AlfredoGreco- remote get-url origin >> repogit/UTNFRA_SO_1P2C_2024_-AlfredoGreco-/RTA_ARCHIVOS_Examen_20241015/Filtro_Avanzado.txt
