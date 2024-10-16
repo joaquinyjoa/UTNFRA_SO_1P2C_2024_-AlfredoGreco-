bd1f1f2929fffe674f367153375e94ff0c1fc857
joaquinyjoa <joaquinalfredogreco@gmail.com>
2024-10-16 02:11:11 +0000

    Agregue el chasis

--- RTA_SCRIPT_Examen_20241015/Punto_E.sh
+++ RTA_SCRIPT_Examen_20241015/Punto_E.sh
@@ -1,1 +1,2 @@
 grep MemTotal:  /proc/meminfo > repogit/UTNFRA_SO_1P2C_2024_-AlfredoGreco-/RTA_ARCHIVOS_Examen_20241015/Filtro_Basico.txt
+sudo dmidecode -t chassis | grep Manufacturer >> repogit/UTNFRA_SO_1P2C_2024_-AlfredoGreco-/RTA_ARCHIVOS_Examen_20241015/Filtro_Basico.txt
