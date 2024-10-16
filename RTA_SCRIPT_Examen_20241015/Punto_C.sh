921952d2a547c750896622d3a18aae90bcb59217
joaquinyjoa <joaquinalfredogreco@gmail.com>
2024-10-16 01:44:37 +0000

    Tercer permiso

--- RTA_SCRIPT_Examen_20241015/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241015/Punto_C.sh
@@ -9,3 +9,4 @@ sudo chmod 750 /Examenes-UTN/alumno_1
 sudo chown p1c2_2024_A2:p1c2_2024_A2 /Examenes-UTN/alumno_2
 sudo chmod 700 /Examenes-UTN/alumno_2
 sudo chown p1c2_2024_A3:p1c2_2024_A3 /Examenes-UTN/alumno_3
+sudo chmod 775 /Examenes-UTN/alumno_3
