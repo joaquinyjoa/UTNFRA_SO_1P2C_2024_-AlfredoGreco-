ff2eed46ab91ff04438ab8a7d4233d839a0b5b8c
joaquinyjoa <joaquinalfredogreco@gmail.com>
2024-10-16 01:38:49 +0000

    Primer dueño

--- RTA_SCRIPT_Examen_20241015/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241015/Punto_C.sh
@@ -4,3 +4,4 @@ sudo useradd -m -s /bin/bash -G p1c2_2024_gAlumno p1c2_2024_A1
 sudo passwd p1c2_2024_A1
 sudo useradd -m -s /bin/bash -G p1c2_2024_gAlumno -p $y$j9T$bh5YqA58p2TOeYKRpitlU1$U.cspPLScu.WOCf2pqFDX5/WdoRu0lzqEgO002xQVQ9 p1c2_2024_A2
 sudo useradd -m -s /bin/bash -G p1c2_2024_gAlumno -p $y$j9T$bh5YqA58p2TOeYKRpitlU1$U.cspPLScu.WOCf2pqFDX5/WdoRu0lzqEgO002xQVQ9 p1c2_2024_A3
+sudo chown p1c2_2024_A1:p1c2_2024_A1 /Examenes-UTN/alumno_1
